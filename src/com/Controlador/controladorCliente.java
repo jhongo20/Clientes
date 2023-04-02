da7b7e35ca4f88235b3aec23d560571669b01450
Jhon Jairo Pérez <jhon16_39@hotmail.com>
2023-04-02 15:28:07 -0500

    Se documenta el controladorCliente

--- src/com/Controlador/controladorCliente.java
+++ src/com/Controlador/controladorCliente.java
@@ -11,15 +11,18 @@ import javax.swing.JOptionPane;
 /**
  *
  * @author JHON
+ * Controlador Cliente
  */
 public class controladorCliente {
+    //Se inicializa el modelo Cliente y el modelo ConsultaClientes que contiene los metodos
     private Cliente cliente;
     private ConsultaClientes operacion;
     
+    //Metodo Registrar
     public void registrarCliente(Cliente c){
-        
+        //Se inicializa el metodo ConsultaClientes
         operacion = new ConsultaClientes();
-        
+        //se valida si el registro fue exitoso o no para retornar el mensaje correspondiente
         if (operacion.registrar(c)) {
             JOptionPane.showMessageDialog(null, "Cliente registrado con exito!");
         }else{
@@ -28,21 +31,25 @@ public class controladorCliente {
 
     }
     
-    public Cliente buscar(Cliente cliente){
-                
-        Cliente c = new Cliente();
+    //Metodo Buscar
+    public Cliente buscar(Cliente c){
+        //Se inicializa el modelo  Cliente       
+        cliente = new Cliente();
+        //Se inicializa el modelo ConsultaClientes
         operacion = new ConsultaClientes();
-        c = operacion.buscar(cliente);
-        
-        System.out.println("nombre Controller: " + cliente.getNombre());
-        
-        return c;
+        //se asigna en la variable cliente el objeto obtenido de la consulta
+        cliente = operacion.buscar(c);
+        //retorna el cliente         
+        return cliente;
         
     }
     
+    //Metodo Actualizar
     public void ActualizarCliente(Cliente c){
+        //Se inicializa el metodo ConsultaClientes
         operacion = new ConsultaClientes();
-        System.out.println("cliente: " + c.getId());
+        //Se valida si el resultado del metodo modificar es true o false para 
+        //retornar el mensaje correspondiente
         if (operacion.modificar(c)) {
             JOptionPane.showMessageDialog(null, "Cliente Actualizado con exito!");
         }else{
@@ -50,8 +57,11 @@ public class controladorCliente {
         }
     }
     
+    //Metodo Eliminar
     public boolean eliminarCliente(Cliente c){
+        //Se inicializa el metodo ConsultaClientes
         operacion = new ConsultaClientes();
+        //se valida si el metodo eliminar es true o false para retornar el mensaje correspondiente
         if (operacion.eliminar(c)) {
             return true;
         }else
